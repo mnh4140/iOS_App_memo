a007f83834136980ee0c46854120772a30bc2e32
NH <mnh4140@gmail.com>
2025-02-26 14:52:59 +0900

    Initial Commit

---
+++ iOS_App_memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_App_memo
+//
+//  Created by NH on 2/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_App_memoTests/iOS_App_memoTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_memoTests.swift
+//  iOS_App_memoTests
+//
+//  Created by NH on 2/26/25.
+//
+
+import Testing
+@testable import iOS_App_memo
+
+struct iOS_App_memoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
